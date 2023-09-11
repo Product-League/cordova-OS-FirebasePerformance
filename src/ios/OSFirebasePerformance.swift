05ad1df84a25468f71b4d38ad08c821956766245
psolipaPL <126564692+psolipaPL@users.noreply.github.com>
2023-09-11 16:42:07 +0100

    Update OSFirebasePerformance.swift

--- src/ios/OSFirebasePerformance.swift
+++ src/ios/OSFirebasePerformance.swift
@@ -9,8 +9,6 @@ class OSFirebasePerformance : CDVPlugin {
     
     override func pluginInitialize() {
         plugin = FirebasePerformancePlugin()
-        FirebaseApp.configure()
-        NSLog('Starting Firebase Performance Plugin')
     }
     
     @objc(startTrace:)
